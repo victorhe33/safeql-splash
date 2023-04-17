c2497984ecde4ef32822fb662b702407c5c85307
Mariko Iwata <3701668+MarikoIwata@users.noreply.github.com>
2023-04-17 16:52:53 -0400

    updated featuresSplit

--- src/components/sections/FeaturesSplit.js
+++ src/components/sections/FeaturesSplit.js
@@ -97,42 +97,16 @@ const FeaturesSplit = ({
 
               <div className="split-item-content center-content-mobile reveal-from-right" data-reveal-container=".split-item">
                 <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
-                  It feels so robust!
+                  circular referencing no more!
                   </div>
                 <h3 className="mt-0 mb-12">
-                  Testing your endpoint's security
-                  </h3>
-                <p className="m-0">
-                Enter the URL of the GraphQL API. You don't need to enter a query string. If a query is entered, it won't affect the results. Choose from the drop down menu and select, Security, click on the Submit button to determine if the query passes or fails the DOS test.
-                  </p>
-              </div>
-
-              <div className={
-                classNames(
-                  'split-item-image center-content-mobile reveal-from-bottom',
-                  imageFill && 'split-item-image-fill'
-                )}
-                data-reveal-container=".split-item">
-                <Image
-                  src={require('./../../assets/images/features-split-image-02.png')}
-                  alt="Features split 02"
-                  width={528}
-                  height={396} />
-              </div>
-            </div>
-
-            <div className="split-item">
-              <div className="split-item-content center-content-mobile reveal-from-left" data-reveal-container=".split-item">
-                <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
-                  No security issues left behind!
-                  </div>
-                <h3 className="mt-0 mb-12">
-                  Schema checking
-                  </h3>
+                Schema checking
+                </h3>
                 <p className="m-0">
                 Once you click onto to the Schema page, there is an codebox for copying your schema in securely, since your schema are only saved for the session. Submit your schema for an interactive table visualization that displays any schema that allow for a circular reference that could be exploited.
                   </p>
               </div>
+
               <div className={
                 classNames(
                   'split-item-image center-content-mobile reveal-from-bottom',
@@ -141,11 +115,13 @@ const FeaturesSplit = ({
                 data-reveal-container=".split-item">
                 <Image
                   src={`https://user-images.githubusercontent.com/3701668/217420191-22bd41b8-f78c-4f12-a730-ebd7df93ad62.gif`}
-                  alt="Features split 03"
+                  alt="Features split 02"
                   width={528}
                   height={396} />
               </div>
             </div>
+
+
 
           </div>
         </div>
