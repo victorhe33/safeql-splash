5836c229609b99cf94172417cab7a5357b111b62
Mariko Iwata <3701668+MarikoIwata@users.noreply.github.com>
2023-04-17 19:48:16 -0400

    updated with pictures

--- src/components/sections/Testimonial.js
+++ src/components/sections/Testimonial.js
@@ -2,6 +2,8 @@ import React from 'react';
 import classNames from 'classnames';
 import { SectionTilesProps } from '../../utils/SectionProps';
 import SectionHeader from './partials/SectionHeader';
+import Image from '../elements/Image';
+
 
 const propTypes = {
   ...SectionTilesProps.types
@@ -60,85 +62,81 @@ const Testimonial = ({
 
             <div className="tiles-item reveal-from-right" data-reveal-delay="200">
               <div className="tiles-item-inner">
-                <div className="testimonial-item-content">
-                  <p className="text-sm mb-0">
-                    — Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum cillum dolore eu fugiat.
-                      </p>
+                <div>
+                  <Image
+                  src={`https://ca.slack-edge.com/T037PK0BFFE-U03CNN093EX-9aadd1873e71-512`}
+                  alt="Features split 02"
+                  width={528}
+                  height={396} />
                 </div>
                 <div className="testimonial-item-footer text-xs mt-32 mb-0 has-top-divider">
                   <span className="testimonial-item-name text-color-high">Victor He</span>
-                  {/* <span className="text-color-low"> / </span>
-                  <span className="testimonial-item-link">
-                    <a href="#0">AppName</a>
-                  </span> */}
+
                 </div>
               </div>
             </div>
 
             <div className="tiles-item reveal-from-bottom" data-reveal-delay="200">
               <div className="tiles-item-inner">
-                <div className="testimonial-item-content">
-                  <p className="text-sm mb-0">
-                    — Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum cillum dolore eu fugiat.
-                      </p>
+                <div>
+                <Image
+                src={`https://user-images.githubusercontent.com/3701668/232609016-f993f9d1-4741-4064-839f-e132b7abc848.jpg`}
+                alt="Features split 02"
+                width={528}
+                height={396} />
                 </div>
                 <div className="testimonial-item-footer text-xs mt-32 mb-0 has-top-divider">
                   <span className="testimonial-item-name text-color-high">Mariko Iwata</span>
-                  {/* <span className="text-color-low"> / </span>
-                  <span className="testimonial-item-link">
-                    <a href="#0">AppName</a>
-                  </span> */}
+                  
                 </div>
               </div>
             </div>
             
             <div className="tiles-item reveal-from-left" data-reveal-delay="200">
               <div className="tiles-item-inner">
-                <div className="testimonial-item-content">
-                  <p className="text-sm mb-0">
-                    — Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum cillum dolore eu fugiat.
-                      </p>
+                <div>
+                <Image
+                  src={`https://ca.slack-edge.com/T037PK0BFFE-U03JW5C6VUN-b4875a36cf88-512 `}
+                  alt="Features split 02"
+                  width={528}
+                  height={396} />
+                  
                 </div>
                 <div className="testimonial-item-footer text-xs mt-32 mb-0 has-top-divider">
                   <span className="testimonial-item-name text-color-high">Christopher Kim</span>
-                  {/* <span className="text-color-low"> / </span>
-                  <span className="testimonial-item-link">
-                    <a href="#0">AppName</a>
-                  </span> */}
+                  
                 </div>
               </div>
             </div>
 
             <div className="tiles-item reveal-from-right" data-reveal-delay="200">
               <div className="tiles-item-inner">
-                <div className="testimonial-item-content">
-                  <p className="text-sm mb-0">
-                    — Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum cillum dolore eu fugiat.
-                      </p>
+                <div>
+                  <Image
+                src={`https://media.licdn.com/dms/image/D5603AQHyBaNdeEWH5w/profile-displayphoto-shrink_200_200/0/1677365263121?e=1687392000&v=beta&t=2uT8Vld8MHibliYDOYxPtowPjIDLOHqgSq-EMV20ROI`}
+                alt="Features split 02"
+                width={528}
+                height={396} />
                 </div>
                 <div className="testimonial-item-footer text-xs mt-32 mb-0 has-top-divider">
                   <span className="testimonial-item-name text-color-high">Etana Kopin</span>
-                  {/* <span className="text-color-low"> / </span> */}
-                  {/* <span className="testimonial-item-link">
-                    <a href="#0">AppName</a>
-                  </span> */}
+                  
                 </div>
               </div>
             </div>
 
             <div className="tiles-item reveal-from-left" data-reveal-delay="200">
               <div className="tiles-item-inner">
-                <div className="testimonial-item-content">
-                  <p className="text-sm mb-0">
-                    — Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum cillum dolore eu fugiat.
-                      </p>
+                <div>
+                <Image
+                src={`https://ca.slack-edge.com/T037PK0BFFE-U03G8C9UX2A-de77bcbf3590-512`}
+                alt="Features split 02"
+                width={528}
+                height={396} />
                 </div>
                 <div className="testimonial-item-footer text-xs mt-32 mb-0 has-top-divider">
                   <span className="testimonial-item-name text-color-high">Collin Twibell</span>
-                  {/* <span className="text-color-low"> / </span>
-                  <span className="testimonial-item-link">
-                    <a href="#0">AppName</a>
-                  </span> */}
+                  
                 </div>
               </div>
             </div>
